a7d7963bd0040016ee9ddf4814e3912de5937d99
Damian <damianpietka92@gmail.com>
2019-07-06 16:13:06 +0200

    header and footer

--- petit/src/app/app.module.ts
+++ petit/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeModule } from "./modules/home/home.module";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, HomeModule],
+    imports: [BrowserModule, AppRoutingModule, HomeModule, CoreModule],
     providers: [],
     bootstrap: [AppComponent]
 })
